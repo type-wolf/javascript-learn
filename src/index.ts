d4014f3f1d42ad4d77874841864d23697ad457e3
Rui <rui19981120.sns@gmail.com>
2023-09-08 10:51:39 +0900

    Add
    Create development environment

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import { spawn, ChildProcess } from 'child_process';
+
+// Get the path of the file to execute from the command line argument
+
+// Get the path to the file from the 3rd argument
+const dir = process.argv[2];
+const fileName = process.argv[3];
+
+// If the file path is not set
+if (!dir || !fileName) {
+	console.error('❌ The path to the file to be executed is not specified');
+	process.exit(1);
+}
+
+const scriptPath = path.resolve(`dist/${dir}`, fileName);
+
+// File Execution
+const childProcess: ChildProcess = spawn('node', ['-r', 'dotenv/config', scriptPath], {
+	// Share standard input/output of child process with parent process
+	stdio: 'inherit',
+});
+
+// Wait for child process to terminate
+childProcess.on('close', (code) => {
+	if (code !== 0) {
+		console.error(`❌ process terminated with error code ${code}`);
+	}
+});
